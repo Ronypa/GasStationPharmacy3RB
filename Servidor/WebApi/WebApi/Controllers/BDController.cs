9c5e78482a71bc8fa2f135ee9039a00ef0fdb90f
Rolo96 <rarias1820@live.com>
2017-09-28 19:51:18 -0400

    Servidor con conexion a base de datos y scripts de creacion e insercion en BD iniciados

---
+++ Servidor/WebApi/WebApi/Controllers/BDController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using GasStationPharmacyDataAccess;
+
+
+namespace WebApi.Controllers
+{
+    public class BDController : ApiController
+    {
+        public IEnumerable<USUARIO> Get() {
+            Console.Write("s");
+            using (GasStationPharmacyEntities entities = new GasStationPharmacyEntities()){
+                return entities.USUARIOS.ToList();
+            }
+
+        }
+
+
+        public USUARIO Get(string id)
+        {
+            using (GasStationPharmacyEntities entities = new GasStationPharmacyEntities())
+            {
+                return entities.USUARIOS.FirstOrDefault(d => d.email==id);
+            }
+
+        }
+
+    }
+}
