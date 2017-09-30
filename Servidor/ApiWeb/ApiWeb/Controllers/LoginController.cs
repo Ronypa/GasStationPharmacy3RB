d3a61b2bd533caa7131f7bd9e1bd02f60f07ac67
Rolo96 <rarias1820@live.com>
2017-09-29 23:15:27 -0400

    Estructura servidor y pagina web en 2 soluciones aparte

---
+++ Servidor/ApiWeb/ApiWeb/Controllers/LoginController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using ApiWeb.Models;
+
+namespace ApiWeb.Controllers
+{
+    public class LoginController : ApiController
+    {
+
+        public IEnumerable<USUARIO> Get()
+        {
+            using (GasStationPharmacyEntities entities = new GasStationPharmacyEntities())
+            {
+                return entities.USUARIOS.ToList();
+            }
+
+        }
+
+
+        public USUARIO Get(string id)
+        {
+            using (GasStationPharmacyEntities entities = new GasStationPharmacyEntities())
+            {
+                return entities.USUARIOS.FirstOrDefault(d => d.email == id);
+            }
+
+        }
+
+
+    }
+}
---
+++ Servidor/ApiWeb/WebApp/Scripts/script.js
@@ -0,0 +1,17 @@
+﻿var url = "/ApiWeb/api/login"
+var app = angular.module("app", []);
+
+var MainController = function ($scope, $http) {
+
+    var onSucess = function (response) { $scope.users = response.data };
+
+    var onFailure = function (reason) { $scope.error = reason };
+
+    var getUsers = function () {
+        $http.get(url)
+            .then(onSucess, onFailure)
+    };
+    getUsers();
+};
+
+app.controller("MainController", MainController);
