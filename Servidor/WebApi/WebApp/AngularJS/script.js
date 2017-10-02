41cd48ae328807f438d90e034bfef444f61cdd19
Ronny23 <ronnyquesada96@gmail.com>
2017-10-01 18:03:38 -0600

    Prueba registrar

--- Servidor/WebApi/WebApp/AngularJS/script.js
+++ Servidor/WebApi/WebApp/AngularJS/script.js
@@ -2,13 +2,22 @@
 
 //Ruteador, (relaciona direccion con html y controlador)
 app.config(function ($routeProvider) {
-    $routeProvider.when('/', {
+    $routeProvider.when('/e', {
         templateUrl: '/AngularJS/Templates/login.html',
         controller: 'logincontroller'
     })
     .otherwise({
         redirectTo: '/'
-    });
+        });
+
+    $routeProvider.when('/', {
+        templateUrl: '/AngularJS/Templates/entra.html',
+        controller: 'entracontroller'
+    })
+        .otherwise({
+            redirectTo: '/'
+        });
+
 });
 
 //Servicio para realizar peticiones http 
@@ -24,6 +33,66 @@ app.service('loginservice', function ($http) {
         return resp;
     };
 });
+
+//Servicio para realizar peticiones http 
+app.service('crearservice', function ($http) {
+
+    this.crear = function (usercrear) {
+        var resp = $http({
+            url: "api/EMPLEADO",
+            method: "POST",
+            data: $.param({
+                Cedula: usercrear.cedula,
+                Nombre1: usercrear.nombre1,
+                Nombre2: usercrear.nombre2,
+                Apellido1: usercrear.apellido1,
+                Apellido2: usercrear.apellido2,
+                Provincia: usercrear.provincia,
+                Cuidad: usercrear.cuidad,
+                Señas: usercrear.señas,
+                FechaNaciento: usercrear.fechaNacimiento,
+                Contraseña: usercrear.contraseña,
+                Sucursal: usercrear.sucursal,
+                Activo: usercrear.activo
+            }),
+            headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+        });
+        return resp;
+    };
+});
+
+
+app.controller('crearcontroller', function ($scope, crearservice) {
+
+    //Funcion para hacer login 
+    $scope.crear = function () {
+        //Body de la peticion http
+        var userCrear = {
+            cedula: $scope.cedula,
+            nombre1: $scope.nombre1,
+            nombre2: $scope.nombre2,
+            apellido1: $scope.apellido1,
+            apellido2: $scope.apellido2,
+            provincia: $scope.provincia,
+            cuidad: $scope.cuidad,
+            señas: $scope.señas,
+            fechaNacimiento: $scope.fechaNacimiento,
+            contraseña: $scope.contraseña,
+            sucursal: $scope.sucursal,
+            activo: $scope.activo,
+
+        };
+        //Hace uso del servicio y ejecuta la peticion http
+        var promisecrear = crearservice.crear(userCrear);
+        promisecrear.then(function (resp) {//Si se ejecuta la peticion bien
+            alert("Bien");
+        }, function (err) {//Si la peticion no funciono bien
+            alert("ERROR NO SEA ESTUPIDO .l.");
+        });
+    };
+});
+
+
 
 
 //Controlador para el login tiene la funcion que se llama desde el html (hace uso del servicio "loginService")
