5c4df89de1b40747046730e7096cf2631ec65124
Ronny23 <ronnyquesada96@gmail.com>
2017-10-02 17:22:48 -0600

    Paginas de Administración

--- Servidor/WebApi/WebApp/AngularJS/script.js
+++ Servidor/WebApi/WebApp/AngularJS/script.js
@@ -1,5 +1,6 @@
 ﻿var app = angular.module('app', ['ngRoute']);
 
+
 //Ruteador, (relaciona direccion con html y controlador)
 app.config(function ($routeProvider) {
     $routeProvider.when('/', {
@@ -18,6 +19,42 @@ app.config(function ($routeProvider) {
             templateUrl: "AngularJS/Templates/entro.html",
             controller: "logincontroller"
         })
+        // route for the home page
+        .when('/admin', {
+            //This is to define the templates in our html file
+            templateUrl: 'AngularJS/Templates/Clientes.html',
+            controller: 'clientes'
+        })
+
+        // route for the about page
+        .when('/doctores', {
+            templateUrl: 'AngularJS/Templates/Doctores.html',
+            controller: 'doctores'
+        })
+
+        // route for the contact page
+        .when('/medicamentos', {
+            templateUrl: 'AngularJS/Templates/Medicamentos.html',
+            controller: 'medicamentos'
+        })
+        // route for the about page
+        .when('/roles', {
+            templateUrl: 'AngularJS/Templates/Roles.html',
+            controller: 'roles'
+        })
+
+        // route for the contact page
+        .when('/sucursales', {
+            templateUrl: 'AngularJS/Templates/sucursales.html',
+            controller: 'sucursales'
+        })
+
+        // route for the contact page
+        .when('/estadisticas', {
+            templateUrl: 'AngularJS/Templates/estadisticas.html',
+            controller: 'estadisticas'
+        })
+
         .otherwise({
             redirectTo: '/'
         });
@@ -62,8 +99,8 @@ app.service('ServicioHTTP', function ($http) {
 
 
 //Controlador para el login tiene la funcion que se llama desde el html (hace uso del servicio "loginService")
-app.controller('logincontroller', function ($scope, ServicioHTTP, $location) {
-
+app.controller('logincontroller', function ($scope, $rootScope, ServicioHTTP, $location) {
+    $rootScope.hideit = true;
     $scope.loginAdministrador = function () {
         var apiRoute = '/WebApi/api/EMPLEADO';
         var student = ServicioHTTP.getLogin(apiRoute, $scope.loginCedula, $scope.loginPassword);
@@ -94,4 +131,38 @@ app.controller('logincontroller', function ($scope, ServicioHTTP, $location) {
         $location.path("/logincliente");
     };
 
+});
+
+// creating the controller and also injecting Angular's $scope
+app.controller('clientes', function ($scope, $rootScope) {
+    // create a message to display in our view
+    $rootScope.hideit = false;
+    $scope.message = 'What appears in the Home Page';
+});
+
+app.controller('doctores', function ($scope, $rootScope) {
+    $rootScope.hideit = false;
+    $scope.message = 'The contenet in the About page.';
+});
+
+app.controller('medicamentos', function ($scope, $rootScope) {
+    $rootScope.hideit = false;
+    $scope.message = 'This is our News Feed.';
+});
+
+// creating the controller and also injecting Angular's $scope
+app.controller('roles', function ($scope, $rootScope) {
+    $rootScope.hideit = false;
+    // create a message to display in our view
+    $scope.message = 'What appears in the Home Page';
+});
+
+app.controller('sucursales', function ($scope, $rootScope) {
+    $rootScope.hideit = false;
+    $scope.message = 'The contenet in the About page.';
+});
+
+app.controller('estadisticas', function ($scope, $rootScope) {
+    $rootScope.hideit = false;
+    $scope.message = 'This is our News Feed.';
 });
